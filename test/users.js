362cb65ad3740b4cc1dca3a38fa078deb32da3db
Bokang Motema <BMotema@jrnet.co.za>
2022-08-16 14:17:17 +0200

    Searching for a specific User using User Id

--- test/users.js
+++ test/users.js
@@ -2,11 +2,20 @@ import supertest from 'supertest';
 const  request = supertest ('https://jsonplaceholder.typicode.com/');
 
 
-// Searching for all Users
+// Searching for all Users and get user id
 describe('users', () =>{
     it('GET /users', () =>{
         request
         .get('users').end((err, res) =>{
+            console.log(err);
+            console.log(res.body);
+        });
+    });
+
+    // Search for username "Delphine" using Id
+    it('GET /users/:id', () =>{
+        request
+        .get('users/9').end((err, res) =>{
             console.log(err);
             console.log(res.body);
         });
